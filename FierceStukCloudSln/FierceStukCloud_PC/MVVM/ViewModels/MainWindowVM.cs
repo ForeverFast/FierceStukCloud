4631376da9998f35405c39a42cf9202858b1a9c2
ForeverFast <59344813+ForeverFast@users.noreply.github.com>
2020-05-09 22:06:41 +0300

    Сделал, как вы и просили. Изменил ImageAsycn и VM. И ещё, у меня DataContext для View привязывается в классе DisplayRootRegistry. Поэтому в xaml файле его указывать не надо.

--- FierceStukCloudSln/FierceStukCloud_PC/MVVM/ViewModels/MainWindowVM.cs
+++ FierceStukCloudSln/FierceStukCloud_PC/MVVM/ViewModels/MainWindowVM.cs
@@ -4,6 +4,9 @@ using FierceStukCloud_NetCoreLib.Services.ImageAsyncS;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
 using System.Text;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -29,12 +32,72 @@ namespace FierceStukCloud_PC.MVVM.ViewModels
                 Image = new ImageAsync()
                 {
                     ImageDefault = ImageDefault,
-                    //pack://application:,,,/fsc_icon.png
-                    ImageUri = @"pack://application:,,,/Avicii - Waiting for Love (Original Mix).mp3"
+                    // Если нижнюю строчку расскоменить - будете ловить исключение
+                    //ImageUri = @"C:\Users\ivans\Desktop\c\Avicii - Waiting for Love (Original Mix).mp3"
                 }
             });
-
+            // тут синхронно будет грузить пикчу из файла на ПК
+            Songs.Last().Image.ImageUri = GetImage(@"C:\Users\ivans\Desktop\c\Avicii - Waiting for Love (Original Mix).mp3");
         }
+
+        private BitmapImage GetImage(string url)
+        {
+            TagLib.File file_TAG = TagLib.File.Create(url);
+            try
+            {
+                if (file_TAG.Tag.Pictures.Length >= 1)
+                {
+                    var bin = (byte[])(file_TAG.Tag.Pictures[0].Data.Data); // Конвертация в массив байтов
+                    var kek = file_TAG.Tag.Pictures[0].Type;
+                    var image = System.Drawing.Image.FromStream(new MemoryStream(bin)); // Конвертация в Image WinForm
+                    var bitmapImage = new BitmapImage();
+                    using (var ms = new MemoryStream()) // Создание потока конвертации изображения в BitmapImage
+                    {
+                        image.Save(ms, ImageFormat.Jpeg);
+                        ms.Seek(0, SeekOrigin.Begin);
+                        bitmapImage.BeginInit();
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.StreamSource = ms;
+                        bitmapImage.EndInit();
+                    }
+                    return bitmapImage;
+                }
+                else
+                    return GetDefaultPic();
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    if (file_TAG.Tag.Pictures.Length >= 1)
+                    {
+                        var bin = (byte[])(file_TAG.Tag.Pictures[0].Data.Data); // Конвертация в массив байтов
+
+                        var image = System.Drawing.Image.FromStream(new MemoryStream(bin)); // Конвертация в Image WinForm
+                        var bitmapImage = new BitmapImage();
+                        using (var ms = new MemoryStream()) // Создание потока конвертации изображения в BitmapImage
+                        {
+                            image.Save(ms, ImageFormat.Png);
+                            ms.Seek(0, SeekOrigin.Begin);
+                            bitmapImage.BeginInit();
+                            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                            bitmapImage.StreamSource = ms;
+                            bitmapImage.EndInit();
+                        }
+                        return bitmapImage;
+                    }
+                    else
+                        return GetDefaultPic();
+                }
+                catch (Exception)
+                {
+                    return GetDefaultPic();
+                }
+            }
+        }
+
+        private BitmapImage GetDefaultPic() => new BitmapImage(new Uri("pack://application:,,,/FierceStukCloud_Lib;component/Resources/Em1.png"));
+
 
         public MainWindowVM()
         {
@@ -43,6 +106,9 @@ namespace FierceStukCloud_PC.MVVM.ViewModels
 
 
             LoadImagesCommand = new RelayCommand(LoadImagesExecute, null);
+
+
+
 
             ImageDefault = new BitmapImage(new Uri("pack://application:,,,/FierceStukCloud_NetCoreLib;component/Resources/Images/fsc_icon.png"));
         }
